729d83c6aed1280112405d0a70286e7bf6f48d85
ANAS727189 <anas23khan083@gmail.com>
2025-11-08 18:09:59 +0530

    fix: fixed build error

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,10 @@
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { betterAuth } from "better-auth";
 import clientPromise from "./mongodb";
-
-const dbPromise = clientPromise.then(client => client.db("treasure_hunt"));
+import type { Db } from "mongodb";
 
 export const auth = betterAuth({
-  database: mongodbAdapter(await dbPromise),
+  database: mongodbAdapter(clientPromise.then(client => client.db("treasure_hunt")) as unknown as Db),
   socialProviders: {
         google: { 
             clientId: process.env.GOOGLE_CLIENT_ID as string, 
